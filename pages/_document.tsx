a53a0578997d395fb886df0cda5e3adb83c2cf21
davidkhan4747 <davidkhankhadjayev4747@gmail.com>
2025-03-17 16:12:30 +0500

    google ads

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,19 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16913228979"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-16913228979');
+            `
+          }}
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
