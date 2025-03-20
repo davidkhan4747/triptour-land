c4fb08fd6a1b3f4cc2f87ea26c8b0b692bf86155
davidkhan4747 <davidkhankhadjayev4747@gmail.com>
2025-03-20 22:22:14 +0500

    fix privacy policy 22

--- lib/privacy-policy/index.ts
+++ lib/privacy-policy/index.ts
@@ -1,3 +1,3 @@
 export { content } from './content';
 export { phoneNumbers } from './types';
-export type { PrivacyContent, ContentSection } from './types';
+export type { Agreement, Section } from './types';
